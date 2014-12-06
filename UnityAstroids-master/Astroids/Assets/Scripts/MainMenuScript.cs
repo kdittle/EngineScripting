886ce3dbab79481bdd156c2d552cba04804a1ed2
Kyle <techgamer@comcast.net>
2014-12-05 19:26:40 -0600

    Recommit for updates.

---
+++ UnityAstroids-master/Astroids/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuScript : MonoBehaviour 
+{
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	void OnGUI()
+	{
+		const string instructionText = "Instructions\nUp Arrow to thrust forward \nLeft and Right arrows to rotate \nSpacebar to fire";
+
+		GUI.Label(new Rect(10, 10, 200, 200), instructionText);
+
+		if (GUI.Button (new Rect (10, 75, 200, 50), "Start Game"))
+		{
+			Application.LoadLevel(1);
+		}
+	}
+}
---
+++ UnityAstroids-master/Astroids/Assets/Scripts/Player Scripts/PlayerSpawnScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawnScript : MonoBehaviour 
+{
+
+    private bool _spawnSafe = true;
+
+	// Use this for initialization
+	void Start () 
+    {
+        _spawnSafe = true;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public bool CheckSpawnStatus()
+    {
+        return _spawnSafe;
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "asteroid" || other.gameObject.tag == "enemy")
+        {
+            _spawnSafe = false;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "asteroid" || other.gameObject.tag == "enemy")
+        {
+            _spawnSafe = true;
+        }
+    }
+}
